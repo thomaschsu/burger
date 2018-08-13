d7ee3d7e96196cbbf2373a587e5e2a55957bbc76
Tom Hsu <thomaschsu@gmail.com>
2018-08-13 19:59:35 -0400

    Modified connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "P@ssw0rd89",
+    database: "burgers_db"
+  });
+};
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
