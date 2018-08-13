a3a1113565dcda60ed8260709bead1b0239d74c4
Tom Hsu <thomaschsu@gmail.com>
2018-08-13 00:55:37 -0400

    Initial files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Shake Shack Burger");
+INSERT INTO burgers (burger_name) VALUES ("In-N-Out Burger");
+INSERT INTO burgers (burger_name) VALUES ("McDonalds Big Mac");
