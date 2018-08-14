148c6752ddd00a0a4f15e195ed4e73d8dba0690b
Tom Hsu <thomaschsu@gmail.com>
2018-08-13 22:17:38 -0400

    Modified connection

--- config/connection.js
+++ config/connection.js
@@ -7,18 +7,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "P@ssw0rd89",
+    password: "",
     database: "burgers_db"
   });
 };
-
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "burgers_db"
-// });
 
 // Make connection.
 connection.connect(function(err) {
